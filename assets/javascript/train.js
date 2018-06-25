3b58bb5754183ce7a99a5e82a6fa87732af8efdc
ngbaoan <bambi_19912003@yahoo.com>
2018-06-24 21:43:08 -0700

    get minutes away and next arrival for new train added

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -7,15 +7,37 @@ var config = {
     storageBucket: "train-schedule-95778.appspot.com",
     messagingSenderId: "1083719097111"
   };
-  firebase.initializeApp(config);
 
-var trainName, destination, frequency, firstDeparture
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+var trainName, destination, frequency, departureTime
+var nextArrival, minutesAway
 
 $('#submit-btn').on('click', function() {
     event.preventDefault();
     trainName = $('#train-name').val().trim();
     destination = $('#destination').val().trim();
     frequency = $('#frequency').val().trim();
-    firstDeparture = $('#first-train-time').val().trim();
+    departureTime = $('#first-train-time').val().trim();
+    console.log(departureTime);
+
+    var today = moment().format('YYYY-MM-DD');
+    var momentDeparture = today + ' ' + departureTime;
+    console.log(momentDeparture);
+    var minuteDiff = moment().diff(momentDeparture, 'minutes');
+    console.log(minuteDiff);
     
+    minutesAway = frequency - (minuteDiff%frequency);
+    nextArrival = moment().add(minutesAway, 'minutes').format('hh:mm A');
+    console.log('minutes away: ' + minutesAway);
+    console.log(nextArrival);
+
+    // database.ref().push({
+    //     name: trainName,
+    //     destination: destination,
+    //     frequency: frequency,
+    //     departure: departure,
+    // })
+
 })
