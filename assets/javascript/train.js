20a649d5d2148da47f6f01410401658f979c09ce
ngbaoan <bambi_19912003@yahoo.com>
2018-06-24 22:17:17 -0700

    adding new train to the table from firebase

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -6,38 +6,59 @@ var config = {
     projectId: "train-schedule-95778",
     storageBucket: "train-schedule-95778.appspot.com",
     messagingSenderId: "1083719097111"
-  };
+};
 
-  firebase.initializeApp(config);
-  var database = firebase.database();
+firebase.initializeApp(config);
+var database = firebase.database();
 
 var trainName, destination, frequency, departureTime
 var nextArrival, minutesAway
 
-$('#submit-btn').on('click', function() {
+//when form submitted, add new train info to firebase
+$('#add-train-btn').on('click', function () {
     event.preventDefault();
     trainName = $('#train-name').val().trim();
     destination = $('#destination').val().trim();
     frequency = $('#frequency').val().trim();
     departureTime = $('#first-train-time').val().trim();
-    console.log(departureTime);
 
-    var today = moment().format('YYYY-MM-DD');
-    var momentDeparture = today + ' ' + departureTime;
-    console.log(momentDeparture);
-    var minuteDiff = moment().diff(momentDeparture, 'minutes');
-    console.log(minuteDiff);
-    
-    minutesAway = frequency - (minuteDiff%frequency);
-    nextArrival = moment().add(minutesAway, 'minutes').format('hh:mm A');
-    console.log('minutes away: ' + minutesAway);
-    console.log(nextArrival);
 
-    // database.ref().push({
-    //     name: trainName,
-    //     destination: destination,
-    //     frequency: frequency,
-    //     departure: departure,
-    // })
+    database.ref().push({
+        name: trainName,
+        destination: destination,
+        frequency: frequency,
+        departure: departureTime,
+    })
 
+})
+
+//get trains info from firebase and add to the table
+database.ref().on('value', function (snapshot) {
+    var trains = snapshot.val();
+    console.log(trains);
+    var trainNumber = 0;
+
+    $('tbody').empty();
+    //looping through each train in firebase
+    for (var train in trains) {
+        trainNumber++;
+        var momentDeparture = moment().format('YYYY-MM-DD') + ' ' + trains[train].departure;
+        var minuteDiff = moment().diff(momentDeparture, 'minutes');
+
+        minutesAway = Number(trains[train].frequency) - (minuteDiff % Number(trains[train].frequency));
+        nextArrival = moment().add(minutesAway, 'minutes').format('hh:mm A');
+
+        var trainDiv = $(`
+        <tr>
+            <th scope="row">${trainNumber}</th>
+            <td>${trains[train].name}</td>
+            <td>${trains[train].destination}</td>
+            <td>${trains[train].frequency}</td>
+            <td>${nextArrival}</td>
+            <td>${minutesAway}</td>
+        </tr>
+        `)
+        
+        $('tbody').append(trainDiv);
+    }
 })
